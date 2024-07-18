bdbc08d0f5e5afb962057bbd8affaf9e6dc19d16
Zihan Yang <zihan_yang_1996@outlook.com>
2024-07-18 15:58:30 -0400

    Implemented the movement for tank

--- Source/ToonTanks/Private/Tank.cpp
+++ Source/ToonTanks/Private/Tank.cpp
@@ -5,9 +5,15 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h" 
+#include "Kismet/GameplayStatics.h"
 
 ATank::ATank()
 {
+	// Use the controller rotation yaw
+	// bUseControllerRotationYaw = true;
+	
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->bUsePawnControlRotation = true;
@@ -17,3 +23,76 @@ ATank::ATank()
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 	Camera->bUsePawnControlRotation = false;
 }
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(IMC_Tank, 0);
+		}
+	}
+
+	// Set up input actions
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ATank::Move);
+		EnhancedInputComponent->BindAction(IA_Look, ETriggerEvent::Triggered, this, &ATank::Look);
+	}
+}
+
+void ATank::Move(const FInputActionValue& Value)
+{
+	// Print on screen the value of the input action
+	// if (GEngine)
+	// {
+	// 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Move: %s"), *Value.ToString()));
+	// }
+	// Get the movement vector from the input action value
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// Get yaw rotation from the controller
+	FRotator ControllerRotationYaw(0, GetControlRotation().Yaw, 0);
+
+
+	// Only if the actor not using the controller rotation yaw
+	// Convert the Rotator to Rotation Matrix
+	FRotationMatrix RotationMatrixYaw(ControllerRotationYaw);
+
+	// Get the forward vector and right vector from the Rotation Matrix
+	FVector ForwardVector = RotationMatrixYaw.GetUnitAxis(EAxis::X);
+	FVector RightVector = RotationMatrixYaw.GetUnitAxis(EAxis::Y);
+
+	// Set the rotation of the tank to the movement directions
+	FRotator TargetRotation = FVector(ForwardVector * MovementVector.Y + RightVector * MovementVector.X).Rotation();
+	FRotator NewRotation = FMath::RInterpConstantTo(GetActorRotation(), TargetRotation, GetWorld()->GetDeltaSeconds(), RotationSpeed.X * 10);  // Multiply by 10 to make it faster
+	SetActorRotation(NewRotation);
+
+	
+	/*
+	// Only if the actor using the controller rotation yaw
+	FVector ForwardVector = GetActorForwardVector();
+	FVector RightVector = GetActorRightVector();
+	*/
+	
+	FVector DeltaLocation = ForwardVector * MovementVector.Y * MoveSpeed * GetWorld()->GetDeltaSeconds() + RightVector * MovementVector.X * MoveSpeed * GetWorld()->GetDeltaSeconds();
+	AddActorWorldOffset(DeltaLocation);
+	
+}
+
+void ATank::Look(const FInputActionValue& Value)
+{
+	// Print on screen the value of the input action
+	// if (GEngine)
+	// {
+	// 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Look: %s"), *Value.ToString()));
+	// }
+
+	FVector2D LookVector = Value.Get<FVector2D>();
+	AddControllerYawInput(LookVector.X * RotationSpeed.X * GetWorld()->GetDeltaSeconds());
+	AddControllerPitchInput(LookVector.Y * RotationSpeed.Y * GetWorld()->GetDeltaSeconds());
+}
+
+
--- Source/ToonTanks/Public/Tank.h
+++ Source/ToonTanks/Public/Tank.h
@@ -11,6 +11,9 @@
  */
 class USpringArmComponent;
 class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
 UCLASS()
 class TOONTANKS_API ATank : public ABasePawn
 {
@@ -19,6 +22,8 @@ class TOONTANKS_API ATank : public ABasePawn
 public:
 	ATank();
 
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
 private:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess = "true"))
@@ -26,4 +31,24 @@ private:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess = "true"))
 	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IA_Move;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputAction* IA_Look;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	UInputMappingContext* IMC_Tank;
+
+	void Move(const FInputActionValue& Value);
+
+	void Look(const FInputActionValue& Value);
+
+public:
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float MoveSpeed = 500.0f;
+
+	UPROPERTY(EditAnywhere, Category="Movement")
+	FVector2D RotationSpeed = FVector2D(100.0f, 100.0f);
 };
